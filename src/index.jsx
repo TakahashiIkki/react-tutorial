ae589cf5e54e3e7ecb4eee5d171eeae6be5e56b9
ikkitang <new_ittyandesu@outlook.jp>
2021-12-30 01:45:06 +0900

    tutorialをやるためにルーティングを切った

--- src/index.jsx
+++ src/index.jsx
@@ -1,193 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Concept } from "./concept"; //作成したpage1.jsを読み込んでいる
+import { Tutorial } from "./tutorial";
 
-class SearchBar extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          name="filterText"
-          placeholder="search..."
-          value={this.props.filterText}
-          onChange={(e) => this.props.handleFilterTextChange(e.target.value)}
-        />
-        <p>
-          <label>
-            <input
-              type="checkbox"
-              name="isStockOnly"
-              checked={this.props.isStockOnly}
-              onChange={(e) =>
-                this.props.handleIsStockOnlyChange(e.target.checked)
-              }
-            />
-            Only show products in stock
-          </label>
-        </p>
-      </form>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Tutorial />} />
+          <Route exact path="/concept" element={<Concept />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
-}
-
-class FilterableProductTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filterText: "",
-      isStockOnly: false,
-    };
-    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-    this.handleIsStockOnlyChange = this.handleIsStockOnlyChange.bind(this);
-  }
-
-  handleFilterTextChange(value) {
-    this.setState({ filterText: value });
-  }
-
-  handleIsStockOnlyChange(value) {
-    this.setState({ isStockOnly: value });
-  }
-
-  render() {
-    const filteredProducts = this.props.products
-      .filter((product) => {
-        const filterText = this.state.filterText;
-        if (!filterText) {
-          return true;
-        }
-        return product.name.indexOf(filterText) !== -1;
-      })
-      .filter((product) => {
-        const isStockOnly = this.state.isStockOnly;
-        if (!isStockOnly) {
-          return true;
-        }
-        return product.stocked;
-      });
-
-    return (
-      <>
-        <SearchBar
-          filterText={this.state.filterText}
-          isStockOnly={this.state.isStockOnly}
-          handleFilterTextChange={this.handleFilterTextChange}
-          handleIsStockOnlyChange={this.handleIsStockOnlyChange}
-        />
-        <ProductTable products={filteredProducts} />
-      </>
-    );
-  }
-}
-
-class ProductRow extends React.Component {
-  render() {
-    const product = this.props.product;
-    const name = product.name;
-    const viewName = product.stocked ? (
-      name
-    ) : (
-      <span style={{ color: "red" }}>{product.name}</span>
-    );
-
-    return (
-      <tr>
-        <td>{viewName}</td>
-        <td>{product.price}</td>
-      </tr>
-    );
-  }
-}
-
-class ProductCategoryRow extends React.Component {
-  render() {
-    const category = this.props.category;
-    return (
-      <tr>
-        <th colSpan="2">{category}</th>
-      </tr>
-    );
-  }
-}
-
-class ProductTable extends React.Component {
-  render() {
-    const categorySet = [
-      ...new Set(this.props.products.map((product) => product.category)),
-    ];
-
-    return (
-      <table>
-        <thead>
-          <tr>
-            <td>Name</td>
-            <td>Price</td>
-          </tr>
-        </thead>
-        <tbody>
-          {categorySet.map((category) => {
-            return (
-              <React.Fragment key={category}>
-                <ProductCategoryRow category={category} />
-                {this.props.products
-                  .filter((product) => product.category === category)
-                  .map((product) => {
-                    return <ProductRow key={product.name} product={product} />;
-                  })}
-              </React.Fragment>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  }
-}
-
-function App() {
-  const response = [
-    {
-      category: "Sporting Goods",
-      price: "$49.99",
-      stocked: true,
-      name: "Football",
-    },
-    {
-      category: "Sporting Goods",
-      price: "$9.99",
-      stocked: true,
-      name: "Baseball",
-    },
-    {
-      category: "Sporting Goods",
-      price: "$29.99",
-      stocked: false,
-      name: "Basketball",
-    },
-    {
-      category: "Electronics",
-      price: "$99.99",
-      stocked: true,
-      name: "iPod Touch",
-    },
-    {
-      category: "Electronics",
-      price: "$399.99",
-      stocked: false,
-      name: "iPhone 5",
-    },
-    {
-      category: "Electronics",
-      price: "$199.99",
-      stocked: true,
-      name: "Nexus 7",
-    },
-  ];
-
-  return (
-    <div>
-      <FilterableProductTable products={response} />
-    </div>
-  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/tutorial.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Tutorial extends React.Component {
+  render() {
+    return <h1>チュートリアル開始</h1>;
+  }
+}
