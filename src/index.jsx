1793f001601ccccfca063446dac2404354dfbddc
ikkitang <new_ittyandesu@outlook.jp>
2021-12-29 17:23:54 +0900

    form ... controlled componentを作った

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) => (
-    <li key={number.toString()}>{number}</li>
-  ));
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2,
+    };
 
-  return <ul>{listItems}</ul>;
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      // controlled component
+      <form>
+        <label>
+          Is Going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            value={this.state.isGoing}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Number of guests:
+          <input
+            type="number"
+            name="numberOfGuests"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5];
   return (
     <div>
-      <NumberList numbers={numbers} />
+      <NameForm />
     </div>
   );
 }
